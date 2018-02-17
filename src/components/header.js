768bce9bb0b95bec3eedfa87b0069ae0fdf5d9c7
Taehoon Koo <liketaehoon@happytechnology.kr>
2018-02-17 19:25:16 +0900

    L24 styling with react-native

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-const Header = () => <Text>Albums!</Text>;
+const Header = () => {
+  const { textStyle } = styles;
+  return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20,
+  },
+};
 
 export default Header;
