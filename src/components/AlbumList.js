7f7fa9a67a3efef2c9d9b352e5ac2d1bfdf44673
Taehoon Koo <liketaehoon@happytechnology.kr>
2018-02-18 16:48:43 +0900

    #L35. Component State

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,12 +3,23 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  // other tutorial teach you using constructor and other ways but basically it's do same things.
+  state = { albums: [] }; // step1.
+
+  // L35
+  // step1. initial (or default) state using class level properties.
+  // step2. fetching data and update state
+  // step3. adopting state
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data })); // step2.
+    // setState is already implemented for Class based Component
+    // we never use this.state = blahblah; statement. we'll talke about it later
   }
 
   render() {
+    console.log(this.state); // will log twice , empty and full data
     return (
       <View>
         <Text>Album List</Text>
