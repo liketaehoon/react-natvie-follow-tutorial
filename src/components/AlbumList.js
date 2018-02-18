37a2f6c235ddd7bb1e2e39c42b338b4dc3b0b954
Taehoon Koo <liketaehoon@happytechnology.kr>
2018-02-18 16:56:22 +0900

    #L37. Rendering a List of Components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,26 +3,22 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
-  // other tutorial teach you using constructor and other ways but basically it's do same things.
-  state = { albums: [] }; // step1.
-
-  // L35
-  // step1. initial (or default) state using class level properties.
-  // step2. fetching data and update state
-  // step3. adopting state
+  state = { albums: [] };
 
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => this.setState({ albums: response.data })); // step2.
-    // setState is already implemented for Class based Component
-    // we never use this.state = blahblah; statement. we'll talke about it later
+      .then(response => this.setState({ albums: response.data }));
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
   render() {
-    console.log(this.state); // will log twice , empty and full data
+    // Now it's time to display list of albums.
+    console.log(this.state);
     return (
       <View>
-        <Text>Album List</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
