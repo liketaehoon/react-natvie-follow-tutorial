6b25a3a735bbf074af09e2b30db7f64a2cb0de4e
Taehoon Koo <liketaehoon@happytechnology.kr>
2018-02-19 21:18:55 +0900

    #46 images with react-native

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,24 +3,37 @@ import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => (
-  <Card>
-    <CardSection>
-      <View>
-      </View>
-      <View style={styles.textContainer}>
-        <Text>{props.album.title}</Text>
-        <Text>{props.album.artist}</Text>
-      </View>
-    </CardSection>
-  </Card>
-);
+const AlbumDetail = ({ album }) => {
+  // teacher like this style.
+  const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyles, textContainer } = styles;
+  return (
+    <Card>
+      <CardSection>
+        <View>
+          <Image
+            style={thumbnailStyles}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={textContainer}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
+  );
+};
 
 const styles = {
   textContainer: {
     flexDirection: 'column',
     justifyContent: 'space-around',
-  }
+  },
+  thumbnailStyles: {
+    height: 50,
+    width: 50,
+  },
 };
 
 export default AlbumDetail;
