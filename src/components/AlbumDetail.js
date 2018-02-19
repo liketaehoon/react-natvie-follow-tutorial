9da1dab3b15a4ab02a7fbcf82b966a693f704921
Taehoon Koo <liketaehoon@happytechnology.kr>
2018-02-19 21:26:42 +0900

    L47 Displaying Album Artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,21 +5,32 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   // teacher like this style.
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyles, textContainer } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyles,
+    textContainer,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyles}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={textContainer}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
       </CardSection>
     </Card>
   );
@@ -30,9 +41,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyles: {
     height: 50,
     width: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alighItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1, // strecth to image width
+    width: null,
   },
 };
 
