c2633259932c9528fc62c2e9648528c379077c12
Taehoon Koo <liketaehoon@happytechnology.kr>
2018-02-19 21:11:16 +0900

    #45 more on styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { Text } from 'react-native';
+  import React from 'react';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = (props) => (
   <Card>
     <CardSection>
-      <Text>{props.album.title}</Text>
+      <View>
+      </View>
+      <View style={styles.textContainer}>
+        <Text>{props.album.title}</Text>
+        <Text>{props.album.artist}</Text>
+      </View>
     </CardSection>
   </Card>
 );
+
+const styles = {
+  textContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  }
+};
 
 export default AlbumDetail;
