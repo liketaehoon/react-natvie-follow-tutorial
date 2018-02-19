72294e01f264c0f0909578960df42788d1891fdb
Taehoon Koo <liketaehoon@happytechnology.kr>
2018-02-19 21:42:45 +0900

    #L50 Styling Button with UX Considerations.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,35 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+  const { buttonStyle, textStyle } = styles;
   return (
-    <TouchableOpacity>
-      <Text>Click Me!!! </Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>
+        Click Me!!!
+      </Text>
     </TouchableOpacity>
   );
+};
+
+const styles = {
+  buttonStyle: {
+    flex: 1, // expand contents as possible as it can
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600', // pretty bold text
+    paddingTop: 10,
+    paddingBottom: 10,
+  }
 };
 
 export default Button;
