78a977aebe98cfdce6856f5ef68b8a4bd4bb8de4
Taehoon Koo <liketaehoon@happytechnology.kr>
2018-02-19 21:30:31 +0900

    #L48 Making Contents Scrollable

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -50,7 +50,7 @@ const styles = {
   },
   thumbnailContainerStyle: {
     justifyContent: 'center',
-    alighItems: 'center',
+    alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
   },
